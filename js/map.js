51e23e60ebdd5d1320eaf81b43a82909156b7f09
Edgar Longat <64935800+longate58@users.noreply.github.com>
2021-10-02 22:58:01 -0400

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -1,34 +1,30 @@
+//---------------------------------------------------
+var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 20,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
 
-    let myMap = L.map('myMap').setView([10.4829, -66.5287], 15)
+    let myMap = L.map('myMap').setView([10.4869, -66.5287], 14)
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { //Fondo del mapa
-    	maxZoom: 60, attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors - Long Art Digital',
+    	maxZoom: 90, attribution: '&copy; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>  contributors - Long Art Digital',
 
     }).addTo(myMap);
 
-    let iconMarker = L.icon({ //Agrega nuestro marcador
+//---------------------------------------------------
+//Agrega nuestro marcador
+    let iconMarker = L.icon({ 
         iconUrl: 'assets/marker.png',
         iconSize: [35, 60],
         iconAnchor: [30, 60]
     })
 
-    let marker2 = L.marker([10.4809, -66.5327], { icon: iconMarker }).addTo(myMap)
+    let marker2 = L.marker([10.4819, -66.5297], { icon: iconMarker }).addTo(myMap)
+L.control.scale().addTo(myMap); 
 
 
 
 
-//------------------------------------------------------
-    // Capas de superposicón
-
-    var MapaBase = {
-      "OpenStreetMap": osm,
-      "Satelital": esri
-    };
-
-    L.control.groupedLayers(MapaBase).addTo(map);
 
 
-//---------------------------------------------------
-var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-});
+
+
