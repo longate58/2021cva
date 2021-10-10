0e7a3549d74bde1aae46878aab71d3bae4575d14
Edgar Longat <64935800+longate58@users.noreply.github.com>
2021-10-10 14:03:44 -0400

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -7,6 +7,17 @@ var osmBase = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 
 
 
+    let myMap = L.map('myMap').setView([10.4757721, -66.5309073], 16)
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { //Fondo del mapa
+      maxZoom: 60, attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors - Long Art Digital',
+
+    }).addTo(myMap);
+
+    let iconMarker = L.icon({ //Agrega nuestro marcador
+        iconUrl: 'img/marker.png',
+        iconSize: [35, 60],
+        iconAnchor: [30, 60]
+    })
 
 
 
