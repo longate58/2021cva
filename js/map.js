0a6b9623d81a0299d20585da2e28befbb0dfada1
Edgar Longat <64935800+longate58@users.noreply.github.com>
2021-10-10 13:55:27 -0400

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -1,3 +1,4 @@
+
 var map = L.map('map').setView([10.4869, -66.5287], 14); // Comuna Ecosocialista Valle Arriba 
 
 var osmBase = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -13,17 +14,7 @@ document.getElementById('select-location').addEventListener('change', function(e
   let coords = e.target.value.split(",");
   map.flyTo(coords, 18);
 })
-//------------------------------------------------------
 
-  //-----------MARCADOR INDICADOR-------------------------------------------
-    let iconMarker = L.icon({ //Agrega nuestro marcador
-        iconUrl: 'assets/marker.png',
-        iconSize: [35, 60],
-        iconAnchor: [30, 60]
-    })
-
-let marker2 = L.marker([10.4769721, -66.5322674], { icon: iconMarker }).addTo(Map)
-//------------------------------------------------------
 
 
 
